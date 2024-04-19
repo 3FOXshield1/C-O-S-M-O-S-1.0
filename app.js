f70abf5ead1c1adf2daf3d3ca1481a3ff1db1636
Khemendra Singh Khangarot <159691221+3FOXshield1@users.noreply.github.com>
2024-04-19 12:21:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ function wishMe(){
     var hour = day.getHours();
 
     if(hour>=0 && hour<12){
-        speak("Good Morning KD...")
+        speak("Good Morning Sir...")
     }
 
     else if(hour>12 && hour<17){
-        speak("Good Afternoon Master...")
+        speak("Good Afternoon Sir...")
     }
 
     else{
